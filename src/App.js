31d33efced4d0ff58c375386ab4d991371e4a3cc
yen-pham <yenpham123456987@gmail.com>
2019-06-21 22:15:02 +0700

    [PHAN-09] 025 Su dung lifeCycle co ban voi mounting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      trangthai : "khoitao"
+    }
+  }
+ 
+ componentWillMount() {
+   console.log('componentWillMount chayj roi');
+ }
+ componentDidMount() {
+   console.log('component DidMount da chay roi');
+ }
+ 
+ 
+  
+  render() {
+    console.log('day la ham render da chay');
+    return (
+     
+      <div className ="App">
+        
+      </div>
+    );
+  }
 }
 
 export default App;
