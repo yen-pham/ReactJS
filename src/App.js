c5bbd4d46f2db6763784ac3261068a7b548bee53
yen-pham <yenpham123456987@gmail.com>
2019-06-21 18:43:52 +0700

    [PHAN-05] 015 Su dung phim tat cho viec tao component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TopMenu from './Component/topmenu/TopMenu';
+import Header from './Component/Header/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <TopMenu/>
+        <Header/>
     </div>
   );
 }
