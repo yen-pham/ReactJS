60808d7dd075a5416e730627f1397056ddd8e96a
yen-pham <yenpham123456987@gmail.com>
2019-06-21 19:13:10 +0700

    [PHAN-05] 016 Hoan thien phan giao dien tu bootstrap sang react js component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import TopMenu from './Component/topmenu/TopMenu';
 import Header from './Component/Header/Header';
+import Content from './Component/Content/Content';
+import Footer from './Component/Footer/Footer';
 
 
 function App() {
@@ -10,6 +12,10 @@ function App() {
     <div className="App">
         <TopMenu/>
         <Header/>
+        <Content vitri="order-lg-2" anh="img/01.jpg" tieude="For those about to rock..." trichdan="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati."/>
+        <Content  anh="img/02.jpg" tieude="We salute you!" trichdan="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati."/>
+        <Content vitri="order-lg-2" anh="img/03.jpg" tieude="Let there be rock!" trichdan="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati."/>
+        <Footer/>
     </div>
   );
 }
---
+++ src/Component/Content/Content.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class Content extends Component {
+    render() {
+        return (
+            <section>
+            <div className="container">
+              <div className="row align-items-center">
+                <div className={"col-lg-6 "+this.props.vitri}>
+                  <div className="p-5">
+                    <img className="img-fluid rounded-circle" src= {this.props.anh} alt />
+                  </div>
+                </div>
+                <div className="col-lg-6 order-lg-1" >
+                  <div className="p-5">
+                    <h2 className="display-4">{this.props.tieude}</h2>
+                    <p> {this.props.trichdan}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        );
+    }
+}
+
+export default Content;
