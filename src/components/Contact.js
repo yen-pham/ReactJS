54aeb88143f29a3d087390216a5ae22d3e17639c
yen-pham <yenpham123456987@gmail.com>
2019-06-24 19:35:44 +0700

    [PHAN-17] 049 Xu ly phan the select trong react form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,8 @@ class Contact extends Component {
     constructor(props){
       super(props);
       this.state ={
-        isRedirect : false
+        isRedirect : false,
+        fNgay : "thu7"
       }
     }
     submitForm = (event) => {
@@ -29,6 +30,7 @@ class Contact extends Component {
       noiDung += " / Email nhan duoc la : "+ this.state.fEmail;
       noiDung += " / Phone nhan duoc la : "+ this.state.fPhone;
       noiDung += " / Message nhan duoc la : "+ this.state.fMess;
+      noiDung += " / Ngay nhan duoc la : "+ this.state.fNgay;
       return noiDung;
     }
     render() {
@@ -96,6 +98,19 @@ class Contact extends Component {
                 <label>Message</label>
                 <textarea name ="fMess" onChange ={(event) => this.isChange(event)} className="form-control" id="message" rows={5} placeholder="Message" required="required" data-validation-required-message="Please enter a message." defaultValue={""} />
                 <p className="help-block text-danger" />
+              </div>
+            </div>
+            <div className="control-group">
+              <div className="form-group floating-label-form-group controls mb-0 pb-2">
+                <label>Chon ngay</label>
+                
+                  <select value={this.state.fNgay}  className="form-control" name="fNgay" onChange ={(event) => this.isChange(event)}>
+                    <option value ="thu3">Ngày thứ 3</option>
+                    <option value ="thu5">Ngày thứ 5</option>
+                    <option value ="thu7">Ngày thứ 7</option>
+                    <option value ="chunhat">Chủ nhật</option>
+                  </select>
+                
               </div>
             </div>
             <br />
