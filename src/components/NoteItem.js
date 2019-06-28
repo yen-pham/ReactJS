4d14d84637bd3b7fb2ca73db6a93048d8f0a1e5a
yen-pham <yenpham123456987@gmail.com>
2019-06-28 15:42:44 +0700

    [PHAN-44] 130 Xu ly goi du lieu ra danh sach bang redux buoc 2

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -4,16 +4,16 @@ class NoteItem extends Component {
     render() {
         return (
             <div className="card ">
-                <div className="card-header shadow p-4 mb-4 bg-secondary " role="tab" id="notee1">
+                <div className="card-header shadow p-4 mb-4 bg-secondary " role="tab" id={"notee"+this.props.id}>
                   <h5 className="mb-0">
-                    <a data-toggle="collapse" data-parent="#accordianId" href="#note1" aria-expanded="true" aria-controls="note1">
-                      Ghi chú ngày 27/6/2019
+                    <a data-toggle="collapse" data-parent="#accordianId" href={"#note"+this.props.id} aria-expanded="true" aria-controls={"note"+this.props.id}>
+                      {this.props.noteTitle}
                     </a>
                   </h5>
                 </div>
-                <div id="note1" className="collapse in" role="tabpanel" aria-labelledby="note1">
+                <div id={"note"+this.props.id} className="collapse in" role="tabpanel" aria-labelledby={"note"+this.props.id}>
                   <div className="card-body">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam animi aut temporibus distinctio, fuga alias ratione odio nemo saepe cumque assumenda fugit dicta incidunt, mollitia ab cum consequatur, esse impedit.
+                  {this.props.noteContent}
                   </div>
                 </div>
               </div>
--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -6,7 +6,7 @@ class NoteList extends Component {
   constructor(props) {
     super(props);
     this.state={
-      dataFirebase :{}
+      dataFirebase :[]
     }
   }
   
@@ -31,20 +31,48 @@ class NoteList extends Component {
       });
     })
   }
+
+  // componentWillMount() {
+  //   var arrayData =[]
+  //   noteData.once('value').then(function(notes){
+  //     notes.forEach(element => {
+  //       const key =element.key;
+  //       const noteTitle =element.val().noteTitle;
+  //       const noteContent =element.val().noteContent;
+  //       arrayData.push({
+  //         id :key,
+  //         noteTitle:noteTitle,
+  //         noteContent:noteContent
+  //       });
+       
+
+  //     });
+     
+  //   })
+  //   this.setState(    
+  //     {dataFirebase:arrayData}
+  // )
+  // }
   
   getData  = () => {
     if(this.state.dataFirebase){
-      console.log(this.state.dataFirebase);
+       return this.state.dataFirebase.map((value,key)=>{
+            return (
+              <NoteItem key={key} id={key} noteTitle={value.noteTitle} noteContent={value.noteContent} />
+            )
+            })
     }
+    
+      
+    
   }
     render() {
         return (
             <div className="col">
             <div id="accordianId" role="tablist" aria-multiselectable="true">
-              <NoteItem/>
-              <NoteItem/>
-              <NoteItem/>
-              <NoteItem/>
+              {
+                this.getData()
+              }
               
             </div>
           </div>
