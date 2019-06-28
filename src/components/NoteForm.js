3e666a64ff5b8d561c607eef3412d0667e532177
yen-pham <yenpham123456987@gmail.com>
2019-06-28 12:48:56 +0700

    [PHAN-42] 126 Ba buoc co ban ket noi store va component

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
 class NoteForm extends Component {
     constructor(props) {
@@ -18,11 +19,12 @@ class NoteForm extends Component {
     }
 
     addData  = (title,content) => {
-        var item ={};
-        item.noteTitle=title;
-        item.noteContent=content;
-        this.props.getData(item);
-        alert('ban da them thanh cong');
+        // var item ={};
+        // item.noteTitle=title;
+        // item.noteContent=content;
+        // this.props.getData(item);
+        // alert('ban da them thanh cong');
+        this.props.addData();
     }
     render() {
         return (
@@ -44,5 +46,16 @@ class NoteForm extends Component {
         );
     }
 }
-
-export default NoteForm;
+const mapStateToProps = (state, ownProps) => {
+    return {
+        testThoi: state.testThoi
+    }
+}
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        addData: () => {
+            dispatch({type:"ADD_DATA"})
+        }
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(NoteForm);
--- src/components/store.js
+++ src/components/store.js
@@ -1,11 +1,16 @@
 var redux = require('redux');
-var store = redux.createStore(allReducer);
-const noteInitialState = {}
+
+const noteInitialState = {
+    testThoi : 'day la test'
+}
 const allReducer = (state = noteInitialState, action) => {
     switch (action.type) {
         case 'ADD_DATA':
+            console.log('Ket noi thanh cong addData');
             return state
         default:
             return state
     }
 }
+var store = redux.createStore(allReducer);
+export default store;
